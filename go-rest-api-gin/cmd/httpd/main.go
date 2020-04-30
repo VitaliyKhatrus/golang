fd871e6d6f0ca1fea3071ccf17dde8cb97eefd29
Vitaliy Khatrus <vitaliy.khatrus@gmail.com>
2020-04-30 15:35:20 +0300

    added new functions

--- go-rest-api-gin/cmd/httpd/main.go
+++ go-rest-api-gin/cmd/httpd/main.go
@@ -1,12 +1,18 @@
 package main
 
 import (
-	"github.com/VitaliyKhatrus/go-rest-api-gin/cmd/httpd/handler"
+	"github.com/VitaliyKhatrus/golang/go-rest-api-gin/cmd/httpd/handler"
+	"github.com/VitaliyKhatrus/golang/go-rest-api-gin/cmd/platform/newsfeed"
 	"github.com/gin-gonic/gin"
 )
 
 func main() {
+	feed := newsfeed.New()
+
 	r := gin.Default()
 	r.GET("/ping", handler.PingGet)
+	r.GET("/newsfeed", handler.NewsfeedGet(feed))
+	r.POST("/newsfeed", handler.NewsfeedPost(feed))
+
 	r.Run() // listen and serve on 0.0.0.0:8080 (for windows "localhost:8080")
 }
---
+++ go-rest-api-gin/go.mod
@@ -0,0 +1,5 @@
+module github.com/VitaliyKhatrus/golang/go-rest-api-gin
+
+go 1.14
+
+require github.com/gin-gonic/gin v1.6.2
